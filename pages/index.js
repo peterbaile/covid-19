ad4a80a6e950f4bfedfd4b2771beb3a36ff69903
peterbaile <peterchenwflms@gmail.com>
2020-05-02 00:16:37 -0400

    update og info

--- pages/index.js
+++ pages/index.js
@@ -103,16 +103,16 @@ const Home = () => {
         {/* <link rel="icon" href="/favicon.ico" /> */}
 
         <meta property="og:title" content="COVID-19 | The Daily Pennsylvanian"/>
-        <meta property="og:image" content="https://snworksceo.imgix.net/dpn/9ae93a96-5757-4a4c-b43e-299c186a6f92.sized-1000x1000.png" />
-        <meta property="og:description" content="Your guide to living at Penn"/>
+        {/* <meta property="og:image" content="https://snworksceo.imgix.net/dpn/9ae93a96-5757-4a4c-b43e-299c186a6f92.sized-1000x1000.png" /> */}
+        {/* <meta property="og:description" content="Your guide to living at Penn"/> */}
         <meta property="og:type" content="article" />
-        <meta property="og:url" content="https://projects.thedp.com/2019/10/housing-guide/"/>
+        <meta property="og:url" content="https://thedp-covid-19.herokuapp.com/"/>
 
         <meta name="twitter:card" content="summary_large_image"/>
         <meta name="twitter:title" content="COVID-19 | The Daily Pennsylvanian"/>
-        <meta name="twitter:image" content="https://snworksceo.imgix.net/dpn/9ae93a96-5757-4a4c-b43e-299c186a6f92.sized-1000x1000.png" />
-        <meta name="twitter:description" content="Your guide to living at Penn"/>
-        <meta name="twitter:url" content="https://projects.thedp.com/2019/10/housing-guide/"/>
+        {/* <meta name="twitter:image" content="https://snworksceo.imgix.net/dpn/9ae93a96-5757-4a4c-b43e-299c186a6f92.sized-1000x1000.png" /> */}
+        {/* <meta name="twitter:description" content="Your guide to living at Penn"/> */}
+        <meta name="twitter:url" content="https://thedp-covid-19.herokuapp.com/"/>
         <meta name="twitter:site" content="@dailypenn"/>
       </Head>
 
