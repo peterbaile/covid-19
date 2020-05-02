d48e30c44d26b42084ff2b3a845285b1fdd84610
peterbaile <peterchenwflms@gmail.com>
2020-05-01 23:25:16 -0400

    add nav bar and latest section

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import '../styles/index.css'
+
+const MyApp = ({ Component, pageProps }) =>
+  <Component {...pageProps} />
+
+export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,34 @@
 import Head from 'next/head'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import s from 'styled-components'
+
+const Background = s.div`
+  background-image: url('/img/dark-background.png');
+  border: 1px solid #707070;
+  opacity: 1;
+  text-align: center;
+`
+
+const CoverImg = s.img`
+  width: 50%;
+  padding: 51px;
+`
+
+const Title = s.div`
+  border-left: 12px solid #D12D4A;
+  padding: 0rem 1rem;
+  text-align: left;
+  font-size: 30px;
+  font-family: 'Libre Franklin', sans-serif;
+  letter-spacing: 0px;
+  color: #283033;
+  opacity: 1;
+`
+
+const LatestDiv = s.div`
+  margin-top: 2rem;
+`
 
 const Home = () => {
   const [articles, setArticles] = useState(null)
@@ -14,11 +42,26 @@ const Home = () => {
   }, [])
 
   return (
-    <div className="container">
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Background>
+        <CoverImg src="/img/Covering-COVID.png" className="img-fluid" />
+      </Background>
+
+      <LatestDiv className="container">
+        <div className="row">
+          <div className="col-md">
+            <Title> Latest Stories </Title>
+          </div>
+          <div className="col-md">
+            <Title> Live Updates </Title>
+          </div>
+        </div>
+      </LatestDiv>
 
       {articles && articles[0].abstract}
       
