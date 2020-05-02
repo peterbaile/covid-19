de9cbf104b4400c6e58f22124126c3068fe02829
peterbaile <peterchenwflms@gmail.com>
2020-05-02 00:10:48 -0400

    add header info

--- pages/index.js
+++ pages/index.js
@@ -97,8 +97,23 @@ const Home = () => {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <title>COVID-19 | The Daily Pennsylvanian</title>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+
+        <meta property="og:title" content="COVID-19 | The Daily Pennsylvanian"/>
+        <meta property="og:image" content="https://snworksceo.imgix.net/dpn/9ae93a96-5757-4a4c-b43e-299c186a6f92.sized-1000x1000.png" />
+        <meta property="og:description" content="Your guide to living at Penn"/>
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content="https://projects.thedp.com/2019/10/housing-guide/"/>
+
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:title" content="COVID-19 | The Daily Pennsylvanian"/>
+        <meta name="twitter:image" content="https://snworksceo.imgix.net/dpn/9ae93a96-5757-4a4c-b43e-299c186a6f92.sized-1000x1000.png" />
+        <meta name="twitter:description" content="Your guide to living at Penn"/>
+        <meta name="twitter:url" content="https://projects.thedp.com/2019/10/housing-guide/"/>
+        <meta name="twitter:site" content="@dailypenn"/>
       </Head>
 
       <Background>
