91968628fa7aaa022bb9391b7e029df4bfac91ca
peterbaile <peterchenwflms@gmail.com>
2020-05-10 18:05:59 -0400

    add og picture

--- components/Header.js
+++ components/Header.js
@@ -8,14 +8,14 @@ const Header = () => (
     {/* <link rel="icon" href="/favicon.ico" /> */}
 
     <meta property="og:title" content="COVID-19 | The Daily Pennsylvanian"/>
-    {/* <meta property="og:image" content="https://snworksceo.imgix.net/dpn/9ae93a96-5757-4a4c-b43e-299c186a6f92.sized-1000x1000.png" /> */}
+    <meta property="og:image" content="https://snworksceo.imgix.net/dpn/ffa1291a-df92-4ed0-8ce5-6ad3f04b082f.sized-1000x1000.png" />
     {/* <meta property="og:description" content="Your guide to living at Penn"/> */}
     <meta property="og:type" content="article" />
     <meta property="og:url" content="https://thedp-covid-19.herokuapp.com/"/>
 
     <meta name="twitter:card" content="summary_large_image"/>
     <meta name="twitter:title" content="COVID-19 | The Daily Pennsylvanian"/>
-    {/* <meta name="twitter:image" content="https://snworksceo.imgix.net/dpn/9ae93a96-5757-4a4c-b43e-299c186a6f92.sized-1000x1000.png" /> */}
+    <meta name="twitter:image" content="https://snworksceo.imgix.net/dpn/ffa1291a-df92-4ed0-8ce5-6ad3f04b082f.sized-1000x1000.png" />
     {/* <meta name="twitter:description" content="Your guide to living at Penn"/> */}
     <meta name="twitter:url" content="https://thedp-covid-19.herokuapp.com/"/>
     <meta name="twitter:site" content="@dailypenn"/>
