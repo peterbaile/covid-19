4774674301c6b17ac46ade2eb3af0059bc2e91f7
jonahcharlton <66034422+jonahcharlton@users.noreply.github.com>
2020-05-29 23:27:07 -0500

    update version 40

--- components/Map.js
+++ components/Map.js
@@ -46,7 +46,7 @@ const Map = () => (
     <MapWrapper
       aria-label="Map"
       id="datawrapper-chart-zlgcC"
-      src="https://datawrapper.dwcdn.net/zlgcC/37/"
+      src="https://datawrapper.dwcdn.net/zlgcC/40/"
       scrolling="no"
       frameborder="0"
     />
