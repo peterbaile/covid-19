cec701061be7d07cb49c7d1f14bb106190d9dc9e
peterbaile <peterchenwflms@gmail.com>
2020-06-20 09:10:18 -0400

    add link to map headers

--- components/Map.js
+++ components/Map.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import s from 'styled-components'
 
-import { Title } from '../components/shared'
+import { Title, StyledLink } from '../components/shared'
 import { LIBRE_BOLD, ROBOTO_REGULAR } from '../utils/font'
 
 const MapWrapper = s.iframe`
@@ -26,6 +26,11 @@ const HeaderText = s.div`
   font-size: 130%;
   color: #464242;
   ${LIBRE_BOLD}
+
+  :hover {
+    color: #B5B4B4;
+    transition: 0.3s;
+  }
 `
 
 const SubText = s.div`
@@ -34,13 +39,24 @@ const SubText = s.div`
   ${ROBOTO_REGULAR}
 `
 
+const LinkTitle = s(Title)`
+  :hover {
+    color: #B5B4B4;
+    transition: 0.3s;
+  }
+`
+
 const Map = () => (
   <div className="container" style={{ marginBottom: '1rem' }} id="tracking">
     <div className="row">
-      <Title> Tracking Fall 2020 </Title>
+      <StyledLink href="https://www.thedp.com/article/2020/05/live-updates-map-latest-news-colleges-opening-coronavirus-national">
+        <LinkTitle> Tracking Fall 2020 </LinkTitle>
+      </StyledLink>
     </div>
     <TextWrapper>
-      <HeaderText> U.S. College 2020 Fall Plans </HeaderText>
+      <StyledLink href="https://www.thedp.com/article/2020/05/live-updates-map-latest-news-colleges-opening-coronavirus-national">
+        <HeaderText> U.S. College 2020 Fall Plans </HeaderText>
+      </StyledLink>
       <SubText> All 50 states, including over 250 colleges, are represented in this map. </SubText>
     </TextWrapper>
     <MapWrapper
